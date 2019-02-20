4f263a9d20b9cbf2a63b4dc23466faa7a325e126
zhouguanyun <zhouguanyun@mychebao.com>
2019-02-20 17:31:20 +0800

    files commit

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 
 import {NativeModules, Platform, DeviceEventEmitter} from 'react-native';
 const RNKit = NativeModules.SRRNKit;
+const RNLogger = NativeModules.SRRNLogger;
 console.log("11111111111111");
 console.log(RNKit);
+console.log("22222222222222");
+console.log(RNLogger);
 if (process.env.NODE_ENV === 'production') {
   RNKit.env = 2;
 } else {
   RNKit.env = 1;
 }
-const RNLogger = NativeModules.SRRNLogger;
-console.log("22222222222222");
-console.log(RNLogger);
 module.exports.RNKit = RNKit;
 module.exports.RNLogger = RNLogger;
